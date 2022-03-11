3394414758a86da35cd7adcf06a40989216166f4
Alanni <2567255916@qq.com>
2022-03-11 08:47:51 +0800

    初始化项目

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Nav, NavContainer, NavLogo, MobileIcon, FaBars, NavMenu, NavItem, NavLinks } from './NavbarElements'
+
+function Navbar() {
+  return (
+    <>
+      <Nav>
+        <NavContainer>
+          <NavLogo to='/'>dolla</NavLogo>
+          <MobileIcon>
+            <FaBars/>
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>About</NavLinks>
+            </NavItem>
+          </NavMenu>
+        </NavContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
