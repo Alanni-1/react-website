78cce0a48b0db3a9e48030288f26571976cb8895
Alanni <2567255916@qq.com>
2022-03-16 09:46:15 +0800

    添加about页面

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,14 @@
+import img from '../../images/svg-1.svg'
+
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  imgStart: false,
+  img,
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+}
--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,10 +1,57 @@
 import React from 'react'
+import {useTranslation} from 'react-i18next'
+import {Button} from '../ButtonElements'
+import {
+  InfoContainer,
+  InfoWrapper,
+  InfoRow,
+  Column1,
+  TextWrapper,
+  TopLine,
+  Heading,
+  Subtitle,
+  BtnWrap,
+  ImgWrap,
+  Column2,
+  Img
+} from './InfoSectionElements'
 
-const InfoSection = () => {
+const InfoSection = ({
+  lightBg,
+  id,
+  imgStart,
+  lightText,
+  darkText,
+  img,
+  alt
+}) => {
+  const {t} = useTranslation()
   return (
-    <div>
-      
-    </div>
+    <>
+      <InfoContainer id={id} lightBg={lightBg}>
+        <InfoWrapper>
+          <InfoRow imgStart={imgStart}>
+            <Column1>
+              <TextWrapper>
+                <TopLine>{t('info.title')}</TopLine>
+                <Heading lightText={lightText}>{t('info.subtitle')}</Heading>
+                <Subtitle darkText={darkText}>{t('info.p')}</Subtitle>
+                <BtnWrap>
+                  <Button to="home" primary='true' dark='true' smooth={true} duration={500} spy={true} exact="true" offset={-80}>
+                    {t('info.button')}
+                  </Button>
+                </BtnWrap>
+              </TextWrapper>
+            </Column1>
+            <Column2>
+              <ImgWrap>
+                <Img src={img} alt={alt}/>
+              </ImgWrap>
+            </Column2>
+          </InfoRow>
+        </InfoWrapper>
+      </InfoContainer>
+    </>
   )
 }
 
